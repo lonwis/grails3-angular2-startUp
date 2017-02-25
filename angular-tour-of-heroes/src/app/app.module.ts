dc560e58de830de86cad2e236a2f188cfa65d59a
Leonard Wisniewski <leonard.wisniewski@michelin.com>
2017-02-25 17:02:01 +0100

    lesson routing,  Navigate to Hero Details

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import  { FormsModule }   from '@angular/forms';
-import  { RouterModule }   from '@angular/router';
 
 
 import { AppComponent }  from './app.component';
@@ -9,30 +8,13 @@ import { HeroDetailComponent }  from './components/hero-detail.component';
 import { HeroesComponent }  from './components/heroes.component';
 import { DashboardComponent }  from './components/dashboard.component';
 import { HeroService }  from './services/hero.service';
+import { AppRoutingModule }  from './app-routing.module';
 
 @NgModule({
   imports:      [
       BrowserModule,
       FormsModule,
-      RouterModule.forRoot([
-        {
-          path: '',
-          redirectTo: 'dashboard',
-          pathMatch: 'full'
-        },
-        {
-          path: 'heroes',
-          component: HeroesComponent
-        },
-        {
-          path: 'dashboard',
-          component: DashboardComponent
-        },
-        {
-          path: 'detail/:id',
-          component: HeroDetailComponent
-        },
-      ])
+      AppRoutingModule,
   ],
   declarations: [
     AppComponent ,
--- angular-tour-of-heroes/src/app/components/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/components/hero-detail.component.ts
@@ -10,17 +10,7 @@ import { HeroService } from '../services/hero.service';
 @Component({
   selector: 'my-hero-detail',
   templateUrl: './hero-detail.component.html',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2> 
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-          <label>name: </label> 
-          <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-       <button (click)="goBack()">Back</button>
-    </div>
-  `,
+  moduleId: module.id,
 })
 export class HeroDetailComponent  implements OnInit{
   @Input()  hero: Hero
