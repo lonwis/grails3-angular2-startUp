3f088778c5a651d5387e5153cdbebcc242b62907
Leonard Wisniewski <leonard.wisniewski@michelin.com>
2017-02-26 12:16:25 +0100

    lesson routing,  Providing HTTP Services Update hero details

--- angular-tour-of-heroes/src/app/components/heroes.component.ts
+++ angular-tour-of-heroes/src/app/components/heroes.component.ts
@@ -30,8 +30,14 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
-  gotoDetail():void {
-    this.router.navigate(['/detail', this.selectedHero.id]);
+  add(name:String):void{
+    name = name.trim()
+    if (!name){return}
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      })
   }
   ngOnInit(): void {
     this.setHeroes();
--- angular-tour-of-heroes/src/app/services/hero.service.ts
+++ angular-tour-of-heroes/src/app/services/hero.service.ts
@@ -7,6 +7,7 @@ import { Hero } from '../components/hero';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  private headers = new Headers({'Content-Type': 'application/json'});
   constructor(private http:Http){}
 
   getHeroes(): Promise<Hero[]> {
@@ -30,5 +31,23 @@ export class HeroService {
     return this.http.get(url)
       .toPromise()
       .then(response => response.json().data as Hero)
-      .catch(this.handleError);  }
+      .catch(this.handleError);
+  }
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+  create(name:string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl,
+            JSON.stringify({name: name}),
+            {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
 }
